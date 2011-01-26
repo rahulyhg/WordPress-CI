c636cac8f95e950c165d49f957fd8f11173ed631
James Brumond <kbjr14@gmail.com>
2011-01-25 18:43:48 -0800

    added hash_hmac() replacement; added _hash(); added empty _salt() function

--- wordpress.php
+++ wordpress.php
@@ -16,6 +16,53 @@
 |
 */
 
+
+if (! function_exists('hash_hmac')) :
+/**
+ * In case hash_hmac doesn't exist, define a replacement
+ *
+ * @link    http://us2.php.net/manual/en/function.hash-hmac.php#93440
+ *
+ * @access  global
+ * @param   string    the algorithm
+ * @param   string    hash data
+ * @param   stirng    secret key
+ * @param   bool      raw output?
+ * @return  string
+ */
+function hash_hmac($algo, $data, $key, $raw_output = false)
+{
+    $algo = strtolower($algo);
+    $pack = 'H'.strlen($algo('test'));
+    $size = 64;
+    $opad = str_repeat(chr(0x5C), $size);
+    $ipad = str_repeat(chr(0x36), $size);
+
+    if (strlen($key) > $size)
+    {
+        $key = str_pad(pack($pack, $algo($key)), $size, chr(0x00));
+    }
+    else
+    {
+        $key = str_pad($key, $size, chr(0x00));
+    }
+
+    for ($i = 0; $i < strlen($key) - 1; $i++)
+    {
+        $opad[$i] = $opad[$i] ^ $key[$i];
+        $ipad[$i] = $ipad[$i] ^ $key[$i];
+    }
+
+    $output = $algo($opad.pack($pack, $algo($ipad.$data)));
+
+    return ($raw_output) ? pack($pack, $output) : $output;
+}
+endif;
+
+
+/**
+ * The WordPress class
+ */
 class Wordpress extends Model {
 	
 	/**
@@ -40,12 +87,22 @@ class Wordpress extends Model {
 	var $_logged_in_cookie = false;
 	
 	/**
+	 * Allow a grace period for POST requests?
+	 *
+	 * @access  public
+	 * @type    bool
+	 */
+	var $_allow_grace = true;
+	
+	/**
 	 * Constructor
 	 */
 	function Wordpress()
 	{
 		$this->CI =& get_instance();
 	}
+
+// ----------------------------------------------------------------------------
 	
 	/**
 	 * Change the table prefix
@@ -214,6 +271,69 @@ class Wordpress extends Model {
 	}
 	
 	/**
+	 * Reads data about the current user
+	 *
+	 * @access  public
+	 * @return  object
+	 */
+	function get_current_user()
+	{
+		return $this->_validate_auth_cookie();
+	}
+	
+	/**
+	 * Sets the current user by ID
+	 *
+	 * @access  public
+	 * @param   int       the id
+	 * @return  bool
+	 */
+	function set_current_user_by_id($id)
+	{
+		// ...
+	}
+	
+	/**
+	 * Sets the current user by username
+	 *
+	 * @access  public
+	 * @param   string    the username
+	 * @return  bool
+	 */
+	function set_current_user_by_username($username)
+	{
+		// ...
+	}
+
+// ----------------------------------------------------------------------------
+	
+	/**
+	 * Figures out the appropriate salt value for a hash
+	 *
+	 * @access  public
+	 * @param   string    the scheme
+	 * @return  string
+	 */
+	function _salt($scheme)
+	{
+		
+	}
+	
+	/**
+	 * Hash a string with hash_hmac
+	 *
+	 * @access  public
+	 * @param   string    the string to hash
+	 * @param   string    the salt scheme
+	 * @return  string
+	 */
+	function _hash($str, $scheme = 'logged_in')
+	{
+		$salt = $this->_salt($scheme);
+		return hash_hmac('md5', $str, $salt);
+	}
+	
+	/**
 	 * Defines the logged in cookie
 	 *
 	 * @access  public
@@ -222,7 +342,7 @@ class Wordpress extends Model {
 	function _define_logged_in_cookie()
 	{
 		$site_url = $this->get_option('site_url');
-		$cookie_hash = $site_url ? md5($cookie_hash) : '';
+		$cookie_hash = $site_url ? md5($site_url) : '';
 		$this->_logged_in_cookie = 'wordpress_logged_in_'.$cookie_hash;
 	}
 	
@@ -239,7 +359,96 @@ class Wordpress extends Model {
 			$this->_define_logged_in_cookie();
 		}
 		
+		// Read the cookie value
+		$cookie = $this->CI->input->cookie($this->_logged_in_cookie, true);
+		if (! $cookie)
+		{
+			return false;
+		}
 		
+		// Seperate the cookie into segments
+		$segments = explode('|', $cookie);
+		if (count($segments) != 3)
+		{
+			return false;
+		}
+		
+		return array_combine(array('username', 'expiration', 'hmac'), $segments);
+	}
+	
+	/**
+	 * Tests for a valid auth cookie
+	 *
+	 * @access  public
+	 * @return  int
+	 */
+	function _validate_auth_cookie()
+	{
+		// Get the cookie segments
+		if (! $segments = $this->_parse_auth_cookie())
+		{
+			return false;
+		}
+		extract($segments, EXTR_OVERWRITE);
+		
+		// Allow POST requests a grace period
+		$expired = $expiration;
+		if ($this->_allow_grace && $this->CI->input->server('REQUEST_METHOD'))
+		{
+			$expired += 3600;
+		}
+		
+		// Check for cookie expiration
+		if ($expired < time())
+		{
+			return false;
+		}
+		
+		// Read the user data
+		if (! $user = $this->get_user($username))
+		{
+			return false;
+		}
+		
+		// Build authentication data
+		$pass_frag = substr($user->user_pass, 8, 4);
+		$key = $this->_hash($username.$pass_frag.'|'.$expiration, 'logged_in');
+		$hash = hash_hmac('md5', $username.'|'.$expiration, $key);
+		
+		// Test for user authenticity
+		if ($hash != $hmac)
+		{
+			return false;
+		}
+		
+		return $user;
+	}
+	
+	/**
+	 * Generate a new auth cookie for a user
+	 *
+	 * @access  public
+	 * @param   mixed     the user/user id
+	 * @return  string
+	 */
+	function _generate_auth_cookie($user)
+	{
+		// Make sure we have a user object
+		if (! is_object($user))
+		{
+			if (! $user = $this->get_user($user))
+			{
+				return false;
+			}
+		}
+		
+		// Generate authentication data
+		$pass_frag = substr($user->user_pass, 8, 4);
+		$key = $this->_hash($user->user_login.$pass_frag.'|'.$expiration, $scheme);
+		$hash = hash_hmac('md5', $user->user_login.'|'.$expiration, $key);
+		
+		// Build the cookie string
+		$cookie = $user->user_login.'|'.$expiration.'|'.$hash;
 	}
 	
 	
